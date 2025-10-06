71c0a41203f26b8a9219e76dbf5ef80e01dad7d4
Vinay Agarwal <vinayagarwal8417@gmail.com>
2025-10-06 20:45:28 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,25 +1,8 @@
-import { Card, Grid, Link, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import Copyright from "./Copyright";
 
 const Footer = () => {
-  return (
-    <Box pb={3}>
-      <Card>
-        <Typography variant="subtitle1">
-          Don't forget to star the{" "}
-          <a
-            href="https://github.com/ihtasham42/social-media-app"
-            target="_blank"
-          >
-            Repo
-          </a>
-          ! ⭐
-        </Typography>
-      </Card>
-    </Box>
-  );
+  return <Box pb={3} />;
 };
 
 export default Footer;
